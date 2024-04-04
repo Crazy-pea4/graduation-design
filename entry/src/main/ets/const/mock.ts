7228ea14f7d9c4f88dcc3e11b5a1059975b96581
Crazy_pea <yarh19898510903@163.com>
2024-04-04 15:45:18 +0800

    feat: 初始化主页结构，新增音乐列表、搜索框、搜索框内容结构

---
+++ entry/src/main/ets/const/mock.ts
@@ -0,0 +1,14 @@
+export const mockData = [
+  {
+    title: '红尘客栈',
+    author: '周杰伦',
+  },
+  {
+    title: '不只是场梦',
+    author: '李玖哲',
+  },
+  {
+    title: '特别的人',
+    author: '方大同',
+  }
+]
