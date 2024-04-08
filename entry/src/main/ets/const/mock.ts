9b90388652cbb06cb7ffd45d6ccabb7a17b08cae
Crazy_pea <yarh19898510903@163.com>
2024-04-08 10:22:41 +0800

    feat: 新增下方音乐播放条、搜索详情页

--- entry/src/main/ets/const/mock.ts
+++ entry/src/main/ets/const/mock.ts
@@ -10,5 +10,29 @@ export const mockData = [
   {
     title: '特别的人',
     author: '方大同',
+  },
+  {
+    title: '红尘客栈',
+    author: '周杰伦',
+  },
+  {
+    title: '不只是场梦',
+    author: '李玖哲',
+  },
+  {
+    title: '特别的人',
+    author: '方大同',
+  },
+  {
+    title: '红尘客栈',
+    author: '周杰伦',
+  },
+  {
+    title: '不只是场梦',
+    author: '李玖哲',
+  },
+  {
+    title: '特别的人',
+    author: '方大同',
   }
 ]
