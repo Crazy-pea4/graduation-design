2aa165d3fbd3af49a44cee69cd22377d538be6ec
Crazy_pea4 <yarh19898510903@163.com>
2024-04-27 22:51:50 +0800

    feat: 实现音频播放

---
+++ entry/src/main/ets/Utils/AVPlayer.ts
@@ -0,0 +1,82 @@
+import media from '@ohos.multimedia.media';
+
+class AVPLAYER {
+  private avPlayer;
+
+  // 注册avplayer回调函数
+  private setAVPlayerCallback() {
+    // seek操作结果回调函数
+    this.avPlayer.on('seekDone', (seekDoneTime) => {
+      console.info(`AVPlayer seek succeeded, seek time is ${seekDoneTime}`);
+    })
+    // error回调监听函数,当avPlayer在操作过程中出现错误时调用reset接口触发重置流程
+    this.avPlayer.on('error', (err) => {
+      console.error(`Invoke avPlayer failed, code is ${err.code}, message is ${err.message}`);
+      this.avPlayer.reset(); // 调用reset重置资源，触发idle状态
+    })
+    // 状态机变化回调函数
+    this.avPlayer.on('stateChange', async (state, reason) => {
+      console.log(state, 'state1111')
+      switch (state) {
+        case 'idle': // 成功调用reset接口后触发该状态机上报
+          console.info('AVPlayer state idle called.');
+          // this.avPlayer.release(); // 调用release接口销毁实例对象
+          break;
+        case 'initialized': // avplayer 设置播放源后触发该状态上报
+          console.info('AVPlayerstate initialized called.');
+          this.avPlayer.prepare().then(() => {
+            console.info('AVPlayer prepare succeeded.');
+          }, (err) => {
+            console.error(`Invoke prepare failed, code is ${err.code}, message is ${err.message}`);
+          });
+          break;
+        case 'prepared': // prepare调用成功后上报该状态机
+          console.info('AVPlayer state prepared called.');
+          this.avPlayer.play(); // 调用播放接口开始播放
+          break;
+        case 'playing': // play成功调用后触发该状态机上报
+          console.info('AVPlayer state playing called.');
+          // this.avPlayer.pause(); // 调用暂停接口暂停播放
+          break;
+        case 'paused': // pause成功调用后触发该状态机上报
+          console.info('AVPlayer state paused called.');
+          // this.avPlayer.play(); // 再次播放接口开始播放
+          break;
+        case 'completed': // 播放结束后触发该状态机上报
+          console.info('AVPlayer state completed called.');
+          // this.avPlayer.stop(); //调用播放结束接口
+          break;
+        case 'stopped': // stop接口成功调用后触发该状态机上报
+          console.info('AVPlayer state stopped called.');
+          // this.avPlayer.reset(); // 调用reset接口初始化avplayer状态
+          break;
+        case 'released':
+          console.info('AVPlayer state released called.');
+          break;
+        default:
+          console.info('AVPlayer state unknown called.');
+          break;
+      }
+    })
+  }
+
+  // 以下demo为使用fs文件系统打开沙箱地址获取媒体文件地址并通过url属性进行播放示例
+  public async createAVPlayerInstance() {
+    // 创建avPlayer实例对象
+    this.avPlayer = await media.createAVPlayer();
+    // 创建状态机变化回调函数
+    this.setAVPlayerCallback();
+    console.log('111111111111111111111')
+  }
+
+  public async setAVPlayerUrl() {
+    if(!this.avPlayer.url) {
+      this.avPlayer.url = 'http://music.163.com/song/media/outer/url?id=447925558.mp3';
+    } else {
+      await this.avPlayer.reset();
+      this.avPlayer.url = 'http://music.163.com/song/media/outer/url?id=447925558.mp3';
+    }
+  }
+}
+
+export const AvPlayer = new AVPLAYER()
