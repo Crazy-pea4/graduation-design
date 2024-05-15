0188e7c89b5ad9bb9544723841eafe0ccde7da4f
Crazy_pea4 <1349083260@qq.com>
2024-05-16 00:31:46 +0800

    feat: 优化样式

--- entry/src/main/ets/const/index.ts
+++ entry/src/main/ets/const/index.ts
@@ -1,2 +1,3 @@
 // HttpRequest
-export const BASE_URL: string = 'http://172.20.10.4:3000/api/';
+// export const BASE_URL: string = 'http://172.20.10.4:3000/api/';
+export const BASE_URL: string = 'http://192.168.7.104:3000/api/';
